b04950d71ece1e26d72413e41dd918d43e12b2c5
GONI95 <sanggoni009@gmail.com>
2022-12-09 20:17:17 +0900

    싱글턴패턴 학습

---
+++ src/SingletonPatternExample.kt
@@ -0,0 +1,38 @@
+class Singleton private constructor() {
+    companion object {
+        @Volatile
+        private var instance: Singleton? = null
+
+        @JvmStatic
+        @Synchronized
+        fun getInstance(): Singleton =
+            instance ?: Singleton().also {
+                instance = it
+            }
+
+        /*
+        @JvmStatic
+        fun getInstance(): Singleton =
+            instance ?: synchronized(this) {
+                instance ?: Singleton().also {
+                    instance = it
+                }
+            }
+         */
+
+    }
+}
+
+fun main() {
+    val instance1 = Singleton.getInstance()
+    val instance2 = Singleton.getInstance()
+    val instance3 = Singleton.getInstance()
+
+    println("instance1: ${instance1.hashCode()}")
+    println("instance2: ${instance2.hashCode()}")
+    println("instance3: ${instance3.hashCode()}")
+
+    if (instance1 == instance2 && instance1 == instance3) {
+        println("equals Instance: true")
+    }
+}
